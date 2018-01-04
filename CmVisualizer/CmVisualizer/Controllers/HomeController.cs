5e9df0c92260789502e63e88a849a2ef329b152b
Tomasz Kaniewski <tomaszkaniewski95@gmail.com>
2018-01-04 22:16:20 +0100

    Add: Ugly Settings change

--- CmVisualizer/CmVisualizer/Controllers/HomeController.cs
+++ CmVisualizer/CmVisualizer/Controllers/HomeController.cs
@@ -21,6 +21,16 @@ namespace CmVisualizer.Controllers
     {
         private readonly IHostingEnvironment _hostingEnvironment;
 
+        //public Dictionary<string, int[]> data = new Dictionary<string, int[]>()
+        //{
+        //    { "Project", new int[]{ 1, 2, 3, 4, 5, 6, 7, 8 } },
+        //    { "Namespace", new int[]{ 9, 10, 11, 12, 13, 14, 15, 16 } },
+        //    { "Class", new int[]{ 17, 18, 19, 20, 21, 22, 23, 24 } },
+        //    { "Function", new int[]{ 25, 26, 27, 28, 29, 30, 31, 32 } }
+        //};
+
+        //private string currentTab = "Project";
+
         public HomeController(IHostingEnvironment hostingEnvironment)
         {
             _hostingEnvironment = hostingEnvironment;
@@ -28,6 +38,13 @@ namespace CmVisualizer.Controllers
 
         public IActionResult Index()
         {
+            TempData["currentTab"] = "Project";
+
+            TempData["Project"] =  new int[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+            TempData["Namespace"] = new int[] { 9, 10, 11, 12, 13, 14, 15, 16 };
+            TempData["Class"] = new int[] { 17, 18, 19, 20, 21, 22, 23, 24 };
+            TempData["Function"] = new int[] { 25, 26, 27, 28, 29, 30, 31, 32 };
+
             return View();
         }
 
@@ -40,6 +57,28 @@ namespace CmVisualizer.Controllers
                                                     .Build();
 
             return RedirectToAction("About");
+        }
+
+        public class Ugly
+        {
+            public string Name { get; set; }
+            public int[] Values { get; set; }
+        }
+
+        [HttpPost]
+        public IActionResult ChangeSetting(Ugly ugly)
+        {
+            TempData["currentTab"] = ugly.Name;
+
+            return Json(new { success = TempData.Peek(ugly.Name) });
+        }
+
+        [HttpPost]
+        public IActionResult SaveSetting(Ugly ugly)
+        {
+            TempData[(string)TempData.Peek("currentTab")] = ugly.Values;
+
+            return Json(new { });
         }
 
         public IActionResult Result()
